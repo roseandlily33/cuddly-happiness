0f36f26e733071b24ae600f1d8b55d42bbb61f6e
Victoria Benoit <vrose834@gmail.com>
2023-04-20 15:40:42 -0300

    made the helper

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {User } = require('../models');
+const { User, Post } = require('../models');
 
 //Get all the posts:
 router.get('/', async (req, res) => {

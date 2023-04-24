a1e0b1b9a245f4a03776579a8e0ec21e29ad61e8
Victoria Benoit <vrose834@gmail.com>
2023-04-24 11:05:13 -0300

    Made the comment js

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,13 +1,19 @@
-//const newComment = (event) => {
+const newComment = (event) => {
     event.preventDefault();
-    const commentContent = document.querySelector('#newComment');
-    if(commentContent){
+    const comment_content = document.querySelector('#newComment');
+    if(comment_content){
         const response = fetch('/api/comments', {
             method: 'POST',
-            body: JSON.stringify({comment})
+            body: JSON.stringify({comment_content, post_id}),
+            headers: {
+                'Content-type': 'application/json'
+            }
         })
+        if(response.ok){
+            document.location.reload();
+        } else {
+            alert('Comment not added');
+        }
     }
-
-
-//}
+}
 document.querySelector('.commentForm').addEventListener('submit', newComment);
